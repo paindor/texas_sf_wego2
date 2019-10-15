e1208d06bf9d3fa477ddd1bd67e9251725b2d937
parkjungkwan <pakjkwan@gmail.com>
2019-10-15 18:01:51 +0900

    191015 18:01

--- src/main/java/com/wego/web/controllers/HomeController.java
+++ src/main/java/com/wego/web/controllers/HomeController.java
@@ -24,13 +24,9 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	@Autowired AdminServiceImpl adminService;
-	
 	@GetMapping("/")
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome  {}.", "HomeController");
-		int count = adminService.findTheNumberOfAdmins();
-		model.addAttribute("count", count );
 		return "index";
 	}
 	
