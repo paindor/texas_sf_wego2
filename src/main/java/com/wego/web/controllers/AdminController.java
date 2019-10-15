7919ec2f47eadf55421493f291fbd904914324e0
parkjungkwan <pakjkwan@gmail.com>
2019-10-15 17:16:56 +0900

    191015 17:16

--- src/main/java/com/wego/web/controllers/AdminController.java
+++ src/main/java/com/wego/web/controllers/AdminController.java
@@ -4,13 +4,32 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.wego.web.serviceimpls.AdminServiceImpl;
 import com.wego.web.services.AdminService;
 
 @Controller
+@RequestMapping("/admin/*")
 public class AdminController {
 	private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
-	//@Autowired AdminService adminService;
+	@Autowired AdminServiceImpl adminService;
+	
+	@GetMapping("/count")
+	public String count(Model model) {
+		int count = adminService.findTheNumberOfAdmins();
+		model.addAttribute("count", count);
+		return "index";
+	}
+	
+	@GetMapping("/info")
+	public String findAdminById(@RequestParam("eid") String eid,
+			@RequestParam("pwd") String pwd) {
+		
+		return "index";
+	}
 
 }
--- src/main/java/com/wego/web/domains/AdminDTO.java
+++ src/main/java/com/wego/web/domains/AdminDTO.java
@@ -10,6 +10,6 @@ public class AdminDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
-	private String empNo,eName,job,mgr,hireDate,sal,comm,deptNo;
+	private String eid,pwd,eName,job,mgr,hireDate,sal,comm,deptNo;
 
 }
